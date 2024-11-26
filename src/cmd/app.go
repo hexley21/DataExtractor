0831a693790a94929aa3470e223428359e0733d3
hexley21 <a.kupreishvili.21@gmail.com>
2024-11-26 17:09:04 +0400

    feat: add app execution

--- src/cmd/app.go
+++ src/cmd/app.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/hexley21/data_extractor/cmd/ui/multi_select"
+	"github.com/hexley21/data_extractor/pkg/config"
 	"github.com/hexley21/data_extractor/pkg/extractor"
 	"github.com/hexley21/data_extractor/pkg/serialization"
 	"github.com/hexley21/data_extractor/pkg/util"
@@ -16,26 +17,77 @@ import (
 var (
 	ErrNoFileName = errors.New("no filename provided, shutting down")
 
+	cfg      *config.Config
 	beautify bool
-	rootCmd = &cobra.Command{
-		Use:   "data-extractor [file]",
+)
+
+func Run(c *config.Config) error {
+	cfg = c
+
+	rootCmd := &cobra.Command{
+		Use:   c.CliName + " [file]",
 		Short: "A CLI for extracting specified fields from any file format",
-		Long: `DataExtractor is a CLI app that can extract only provided fields from a file and output the new resulting file.
+		Long: c.AppName + ` is a CLI app that can extract only provided fields from a file and output the new resulting file.
 for now, only JSON extension is supported.`,
 		Args: cobra.ExactArgs(1),
 		RunE: RunE,
 	}
-)
 
-func init() {
 	rootCmd.Flags().BoolVarP(&beautify, "beautify", "b", false, "Enable beautify mode")
-}
 
-func Run() error {
 	return rootCmd.Execute()
 }
 
-
 func RunE(cmd *cobra.Command, args []string) error {
-	return nil
+	fileArg := args[0]
+
+	if fileArg == "" {
+		return ErrNoFileName
+	}
+
+	fileExt := filepath.Ext(fileArg)
+
+	processor, err := serialization.GetProcessor(fileExt, cfg.Beautify[fileExt], beautify)
+	if err != nil {
+		return err
+	}
+
+	if !strings.Contains(fileArg, string(os.PathSeparator)) {
+		fileArg, err = util.PrependCurrentDir(fileArg)
+		if err != nil {
+			return err
+		}
+	}
+
+	content, err := util.ReadFromPath(fileArg)
+	if err != nil {
+		return err
+	}
+
+	var data interface{}
+	err = processor.Deserialize(content, &data)
+	if err != nil {
+		return err
+	}
+
+	keys := extractor.New().Keys(data)
+
+	selected, err := multi_select.DisplayChecklist(keys, cfg.Colors.MultiSelect[fileExt])
+	if err != nil {
+		return err
+	}
+
+	data = extractor.New().Data(data, selected)
+
+	res, err := processor.Serialize(data)
+	if err != nil {
+		return err
+	}
+
+	outputPath, err := util.PrependCurrentDir("extracted_" + filepath.Base(fileArg))
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outputPath, res, 0644)
 }
