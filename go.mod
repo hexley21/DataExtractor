544e6e171b7f40500a8b09c86545445b5f5c4eba
hexley21 <a.kupreishvili.21@gmail.com>
2024-11-29 19:06:26 +0400

    perf(json): move serialization from `std` to `jsoniter`

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.3
 require (
 	github.com/charmbracelet/bubbletea v1.2.3
 	github.com/charmbracelet/lipgloss v1.0.0
+	github.com/json-iterator/go v1.1.12
 	github.com/spf13/cobra v1.8.1
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -19,6 +20,8 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
--- pkg/serialization/json/json.go
+++ pkg/serialization/json/json.go
@@ -1,7 +1,7 @@
 package json
 
 import (
-	"encoding/json"
+	jsoniter "github.com/json-iterator/go"
 	"strings"
 )
 
@@ -10,19 +10,16 @@ type JsonProcessor struct {
 }
 
 func New(indent int) *JsonProcessor {
-	if (indent % 4) == 0 {
-		return &JsonProcessor{strings.Repeat("\t", indent/4)}
-	}
 	return &JsonProcessor{strings.Repeat(" ", indent)}
 }
 
 func (p *JsonProcessor) Serialize(data interface{}) ([]byte, error) {
 	if p.indent == "" {
-		return json.Marshal(data)
+		return jsoniter.Marshal(data)
 	}
-	return json.MarshalIndent(data, "", p.indent)
+	return jsoniter.MarshalIndent(data, "", p.indent)
 }
 
 func (p *JsonProcessor) Deserialize(content []byte, data interface{}) error {
-	return json.Unmarshal(content, data)
+	return jsoniter.Unmarshal(content, data)
 }
